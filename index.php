a1317e00e7932a4e02f56d362a9a4db3694b5f41
andreashovland <andreashovland96@gmail.com>
2021-09-17 13:05:33 +0200

    koblet sammen sidene

--- index.php
+++ index.php
@@ -7,16 +7,14 @@
     <body>
         <img src="bilder/naring.png" alt="logo">
         <h1>Næringsmegleren Sædberg & Hodne</h1>
-        
-        <div class="container">
-            <div class="box">
 
-                <form action="index.php" method="get" id="form1">
-                    <input type="text" name="søk" placeholder="Søk">
-                </form><br>
-                <button type="submit" form="form1" value="submit" onclick="location.href='filter.php'">Filter</button><br>
-                <button type="submit" form="form1" value="submit">Søk</button>
-            </div>
+        <div class="box">
+
+            <form action="filter.php" method="get" id="form1">
+                <input type="text" name="søk" placeholder="Søk">
+            </form><br>
+            <button type="submit" form="form1" value="submit" onclick="location.href='filter.php'">Filter</button><br>
+            <button type="submit" form="form1" value="submit">Søk</button>
         </div>
     </body>
 </html>
