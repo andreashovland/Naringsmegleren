5b0eeeeb6aedbc5e70576ea785aecb7aaf5e59bd
andreashovland <andreashovland96@gmail.com>
2021-09-21 15:48:20 +0200

    prøvd å koble databasen med formen

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
         <div class="header">
             <img src="bilder/logo_senter_hvit.png" class="logo">
         </div>
-        
+
         <div class="box">
 
-            <form action="resultat.php" method="get" id="form1">
+            <form method="post" id="form1">
                 <input type="text" name="søk" placeholder="Søk">
                 <input type="submit" value="Søk" name="søkeknapp">
             </form><br>
@@ -21,3 +21,31 @@
         </div>
     </body>
 </html>
+
+<?php
+
+include('config.php');
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+$sql = "SELECT firmaNavn, adresse, antallAnsatte, bransjeDetalj FROM firma";
+$result = $conn->query($conn, $sql);
+$queryResults = mysqli_num_rows($result);
+
+if ($queryResults > 0) {
+    while ($row = mysqli_fetch_assoc($result)){
+        echo "<table><tr>
+        <th>Firma</th>
+        <th>Adresse</th>
+        <th>Antall ansatte</th>
+        <th>Bransje</th>
+        </tr>
+        <tr>
+        <td>" .$row['firmaNavn']. "</td>
+        <td>" .$row['adresse']. "</td>
+        <td>" .$row['antallAnsatte']. "</td>
+        <td>" .$row['bransjeDetalj']. "</td>
+        </tr></table>";
+    }
+}
+?>
