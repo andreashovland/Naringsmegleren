ae02e0db7dcdbd7d977581d8436b077819babf44
andreashovland <andreashovland96@gmail.com>
2021-10-15 17:17:34 +0200

    tilpasset koden til den nye databasen

--- Test/filtrere.php
+++ Test/filtrere.php
@@ -6,13 +6,14 @@
 <body>
 
 <?php
-    $con = mysqli_connect("localhost", "root", "", "Næringsmegleren");
+    include("config.php");
+    //$conn = mysqli_connect("localhost", "root", "", "NæringsmeglerenDB");
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
     // Henter data fra databasen til dropdowns
-
-    $lokasjonSet = $con->query("SELECT lokasjon FROM lokasjon");
-    $statusSet = $con->query("SELECT status FROM status");
-    $bransjeSet = $con->query("SELECT bransje FROM bransje");
+    $lokasjonSet = $conn->query("SELECT lokasjon FROM lokasjon");
+    $statusSet = $conn->query("SELECT status FROM status");
+    $bransjeSet = $conn->query("SELECT bransje FROM bransje");
 ?>
 
 
@@ -69,10 +70,10 @@
         <label for="adrEndring">Siste adr. endring</label>
         <input type="text" name="adrEndring" placeholder="Senest"><br>
         
-        <!-- Omsetning -->
-        <label for="minOmsetning">Omsetning</label>
-        <input type="text" name="minOmsetning" placeholder="min">
-        <input type="text" name="maxOmsetning" placeholder="max"><br>
+        <!-- Inntekt -->
+        <label for="minInntekt">Omsetning</label>
+        <input type="text" name="minInntekt" placeholder="min">
+        <input type="text" name="maxInntekt" placeholder="max"><br>
         
         <!-- Next -->
         <label for="next">Next?</label>
@@ -88,60 +89,4 @@
 </div>
 </body>
 </html>
-<?php
-/*
-$sql = "SELECT firmaNavn, antallAnsatte, kontaktPerson, kommentarDetalj, fornavn, status
-            FROM firma
-            JOIN kontaktinfo
-            ON kontaktinfo.orgNum=firma.orgNum
-            JOIN firmakommentar
-            ON firmakommentar.orgNum=firma.orgNum
-            JOIN status
-            ON status.statusId=firmakommentar.statusId
-            JOIN ansatte
-            ON ansatte.ansattId=firmakommentar.ansattId
-            JOIN lokasjon 
-            ON lokasjon.lokasjonId=firma.lokasjonId
-            JOIN bransje
-            ON bransje.bransjeId=firma.bransjeId
-            WHERE lokasjon LIKE '%$lokasjon%' and bransje LIKE '%$bransje%'";
 
-
-$result = $con->query($sql);
-            echo "<div class='container'>
-            <table>
-            <tr>
-            <th>Firmanavn</th>
-            <th>Kontaktperson</th>
-            <th>Antall ansatte</th>
-            <th>Status</th>
-            <th>Vår kontaktperson</th>
-            <th>Kommentar</th>
-            <th></th>
-            <th></th>
-            </tr>";
-
-            while ($row = $result->fetch_assoc()){
-
-                echo "<tr>
-                <td>" . $row['firmaNavn'] . "</td>
-                <td>" . $row['kontaktPerson'] . "</td>
-                <td>" . $row['antallAnsatte'] . "</td> 
-                <td>" . $row['status'] . "</td>
-                <td>" . $row['fornavn'] . "</td>
-                <td>" . $row['kommentarDetalj'] . "</td>
-                <td><button class='visMer'>Vis mer</button></td>
-                <td><button class='rediger'>Rediger</button></td>
-                </tr></div>";
-            }*/
-/*
-            <select name="bransje">
-            <?php
-                while ($rows = $bransjeSet->fetch_assoc()){
-                    $bransje = $rows['bransje'];
-                    echo "<option value='$bransje'>$bransje</option>";
-                }
-            ?>
-        </select>
-        */
-?>
