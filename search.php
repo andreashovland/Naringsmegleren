3fc0280f710dd83313098d99b2db835000511484
RikkeSolvang <rikke_solvang@hotmail.com>
2021-11-16 11:25:51 +0100

    log inn for hver side

--- search.php
+++ search.php
@@ -1,3 +1,14 @@
+<?php
+
+session_start();
+
+if($_SESSION["verified"] != true || !isset($_SESSION["verified"])) {
+    header("Location: login.php");
+    exit();
+}
+
+?>
+
 <html>
 <head>
         <title>Næringsmegleren</title>
